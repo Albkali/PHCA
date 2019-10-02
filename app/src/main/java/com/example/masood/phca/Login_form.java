c3206c8d53ea2c7409422ce985d16b29f085777f
masoodhayee <masudmasud87@yahoo.com>
2019-10-02 14:21:28 +0300

    test

---
+++ app/src/main/java/com/example/masood/phca/Login_form.java
@@ -0,0 +1,85 @@
+package com.example.masood.phca;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class Login_form extends AppCompatActivity {
+
+    EditText txtEmail,txtPassword;
+    Button btn_login;
+
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_form);
+
+
+        txtEmail = (EditText)findViewById(R.id.etName);
+        txtPassword = (EditText)findViewById(R.id.etPassword);
+        btn_login = (Button) findViewById(R.id.login_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+               String email = txtEmail.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login_form.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                } else {
+                                    Log.i("PROBEM","Failed");
+                                }
+
+                                // ...
+                            }
+                        });
+
+
+                /*firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login_form.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                                } else {
+                                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    Toast.makeText(Login_form.this,"Login Failed ", Toast.LENGTH_SHORT);
+                                }
+
+                                // ...
+                            }
+                        });*/
+
+            }
+        });
+    }
+
+
+}
--- app/src/main/java/com/example/masood/phca/MainActivity.java
+++ app/src/main/java/com/example/masood/phca/MainActivity.java
@@ -1,67 +1,20 @@
 package com.example.masood.phca;
 
-import android.content.Intent;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Name,Password;
-    private DatabaseReference ref ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        Name = (EditText)findViewById(R.id.etName);
-        Password = (EditText)findViewById(R.id.etPassword);
 
-        ref = FirebaseDatabase.getInstance().getReference().child("Login");
-    }
-    String prName;
-    String prPassword;
-    Users us = new Users();
-    public void bt_button(View view) {
 
-        prName = Name.getText().toString();
-         prPassword = Password.getText().toString();
-
-        if(ref.child(prPassword) != null){
-        ref.child(prPassword).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Users users = dataSnapshot.getValue(Users.class);
-                if(prPassword.equals(us.getPassword())){
-                    Toast.makeText(MainActivity.this, "Login Succesfull", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                    //Intent start = new Intent(MainActivity.this,Start.class);
-                    //finish();
-                    //startActivity(start);
-                }else {
-                    Toast.makeText(MainActivity.this, "Enter Valid Name......", Toast.LENGTH_LONG).show();
-
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-    }else {
-            Toast.makeText(MainActivity.this, "User dosnt exist.......", Toast.LENGTH_LONG).show();
-
-        }
 }}
