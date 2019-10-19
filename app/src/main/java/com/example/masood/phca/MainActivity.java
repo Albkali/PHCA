a03835437d29ec60bd7cc09a5bf6c221d3479271
Yasser Albkali <Yasser.albkali@hotmail.com>
2019-10-20 02:48:33 +0300

    add new  5 drawable files and nav_header.xml  add menu to main_activity

--- app/src/main/java/com/example/masood/phca/MainActivity.java
+++ app/src/main/java/com/example/masood/phca/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.masood.phca;
 
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,16 +16,14 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton calbtn2 ;
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ImageButton calbtn2 ;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         calbtn2= (ImageButton)findViewById(R.id.cal_btn);
         calbtn2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,9 +35,30 @@ public class MainActivity extends AppCompatActivity {
         public void calclick(){
             Intent intent = new Intent(this, cal.class);
             startActivity(intent);
+
+
+
+
+            Toolbar toolbar = findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            drawer = findViewById(R.id.drawer_layout);
+
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                    R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+           drawer.addDrawerListener(toggle);
+           toggle.syncState();
+
         }
 
-
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
     }
