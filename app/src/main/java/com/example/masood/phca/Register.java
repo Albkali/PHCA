34365c3e28583ac5887f00d58ac5d814b207c909
Yasser Albkali <Yasser.albkali@hotmail.com>
2019-10-25 01:14:12 +0300

    change the firebase from realtime database to cloud store database

--- app/src/main/java/com/example/masood/phca/Register.java
+++ app/src/main/java/com/example/masood/phca/Register.java
@@ -11,27 +11,36 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    EditText txtEmail,txtPassword,txtLastName,txtMotherName,txtPhone,txtFirstName;
+    EditText txtEmail,txtPassword,txtLastName,txtMotherName,
+            txtPhone,txtFirstName,txtBirthday,txtHeight  ,txtWeight,
+            txtGender,txtBloodtype, txtSpecialNeed ;
+
     Button btn_next;
     Child child;
-    DatabaseReference reff;
-
-
+    //DatabaseReference reff;
+    FirebaseFirestore db ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
+         db = FirebaseFirestore.getInstance();
         btn_next= (Button)findViewById(R.id.nextregister2);
         txtEmail=(EditText)findViewById(R.id.editTextFirsttName);
         txtPassword=(EditText)findViewById(R.id.editTextPassword);
@@ -40,51 +49,82 @@ public class Register extends AppCompatActivity {
         txtMotherName=(EditText)findViewById(R.id.editTextMatherName);
         txtPhone=(EditText)findViewById(R.id.editTextPhoneNum);
 
+
         child = new Child();
-        reff = FirebaseDatabase.getInstance().getReference().child("child");
+       // reff = FirebaseDatabase.getInstance().getReference().child("Child");
 
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                child.setChildName(txtFirstName.getText().toString().trim());
-                child.setChildLastName(txtLastName.getText().toString().trim());
-                child.setChildMotherName(txtMotherName.getText().toString().trim());
-                child.setEmail(txtEmail.getText().toString().trim());
-                child.setPassword(txtPassword.getText().toString().trim());
+               // child.setChildName(txtFirstName.getText().toString());
+               // child.setChildLastName(txtLastName.getText().toString());
+                //child.setChildMotherName(txtMotherName.getText().toString());
+                //child.setEmail(txtEmail.getText().toString().trim());
+                //child.setPassword(txtPassword.getText().toString().trim());
                 //Number = (txtFirstName.getText().toString().trim());
 
-                reff.push().setValue(child);
-                Toast.makeText(Register.this,"data inserted",Toast.LENGTH_LONG).show();
+                Map<String, Object> child = new HashMap<>();
+                child.put("FirstName", txtFirstName.getText().toString());
+                child.put("LastName", txtLastName.getText().toString());
+                child.put("MatherName", txtMotherName.getText().toString());
+                child.put("Email", txtEmail.getText().toString());
+                child.put("Password", txtPassword.getText().toString());
+                child.put("NPhone", txtPhone.getText().toString());
+                //child.put("Birthday", txtBirthday.getText().toString());
+               // child.put("Height", txtHeight.getText().toString());
+               // child.put("Weight", txtWeight.getText().toString());
 
+                db.collection("Child")
+                        .add(child)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(Register.this, "data inserted", Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent( Register.this, activity_Register1.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Register.this, "data  not inserted", Toast.LENGTH_LONG).show();
+                            }
+                         });
 
             }
-        });
+            //reff.push().setValue(child);
+            // Toast.makeText(Register.this,"data inserted",Toast.LENGTH_LONG).show();
 
-        //btn_register.setOnClickListener(new View.OnClickListener() {
 
-           // @Override
-           // public void onClick(View view) {
+            //}
+            // });
 
-                //String email = txtEmail.getText().toString().trim();
-               // String password = txtPassword.getText().toString().trim();
+            //btn_register.setOnClickListener(new View.OnClickListener() {
 
-              //  firebaseAuth.createUserWithEmailAndPassword(email, password)
-                    //    .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
-                          //  @Override
-                          //  public void onComplete(@NonNull Task<AuthResult> task) {
-                           //     if (task.isSuccessful()) {
-                           //         startActivity(new Intent(getApplicationContext(),MyDrawer.class));
-                          //      } else {
-                          //          Log.i("PROBEM","Failed");
-                            //    }
+            // @Override
+            // public void onClick(View view) {
 
-                                // ...
-                          //  }
-                       // });
-          //  }
-        //});
+            //String email = txtEmail.getText().toString().trim();
+            // String password = txtPassword.getText().toString().trim();
+
+            //  firebaseAuth.createUserWithEmailAndPassword(email, password)
+            //    .addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+            //  @Override
+            //  public void onComplete(@NonNull Task<AuthResult> task) {
+            //     if (task.isSuccessful()) {
+            //         startActivity(new Intent(getApplicationContext(),MyDrawer.class));
+            //      } else {
+            //          Log.i("PROBEM","Failed");
+            //    }
+
+            // ...
+            //  }
+            // });
+            //  }
+            //});
+
+    });
     }
-
 
     public void ClickToRegister1(View view)
     {
