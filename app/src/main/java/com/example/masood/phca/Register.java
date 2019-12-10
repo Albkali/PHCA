ce80110c3b2b09c91ea28850d88c4f3f995e8afb
Yasser Albkali <Yasser.albkali@hotmail.com>
2019-12-10 19:44:08 +0300

    Marge activity_Register1 class  with Register class ,                                                                  Marge activity_Register1 layout with activity_Register layout                                                                  and make registration in one class and layout xml with ScrollView

--- app/src/main/java/com/example/masood/phca/Register.java
+++ app/src/main/java/com/example/masood/phca/Register.java
@@ -1,10 +1,12 @@
 package com.example.masood.phca;
 import android.Manifest;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.example.masood.phca.ui.profile.ProfileFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -27,6 +36,12 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
 import com.google.firebase.storage.UploadTask;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -48,6 +63,21 @@ public class Register extends AppCompatActivity {
    public  Uri imguri ;
 
 
+
+    DatePickerDialog datePicker;
+    Calendar calendar;
+    int day;
+    int month;
+    int year;
+    String BDate;
+    EditText txtBirthday,txtHeight  ,txtWeight;
+    Button DoneRegister;
+    RadioGroup mGender;
+    RadioButton mGenderOptions ;
+    CheckBox SpecialNeeds ;
+    FirebaseUser user;
+    int Heightnumber,Weightnumber;
+
     private static final int PReqCode = 2 ;
     private static final int REQUESCODE = 2 ;
 
@@ -60,7 +90,6 @@ public class Register extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
 
-        btn_next = (Button) findViewById(R.id.nextregister2);
         txtEmail1 = (EditText) findViewById(R.id.editTextEmail);
         txtPassword1 = (EditText) findViewById(R.id.editTextPassword);
         txtFirstName = (EditText) findViewById(R.id.editTextFirsttName);
@@ -69,7 +98,23 @@ public class Register extends AppCompatActivity {
         txtPhone = (EditText) findViewById(R.id.editTextPhoneNum);
         child = new Child();
 
-        btn_next.setOnClickListener(new View.OnClickListener() {
+        calendar = Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+
+        db = FirebaseFirestore.getInstance();
+        txtBirthday = (EditText) findViewById(R.id.editTextBabeBirthday);
+        txtHeight = (EditText) findViewById(R.id.editTextHeight);
+        txtWeight = (EditText) findViewById(R.id.editTextWeight);
+        mGender = (RadioGroup) findViewById(R.id.m_Gender);
+        SpecialNeeds = (CheckBox) findViewById(R.id.checkboxsn);
+
+        DoneRegister = (Button) findViewById(R.id.btn_register);
+
+
+        DoneRegister.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onClick(View view) {
                 InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Register.INPUT_METHOD_SERVICE);
@@ -137,23 +182,98 @@ public class Register extends AppCompatActivity {
                         }
                     });
 
+                    child = new Child();
+                    Spinner spinner = (Spinner) findViewById(R.id.spinnerBloodType);
+                    String typeofblood = spinner.getSelectedItem().toString();
+
+                    Heightnumber = Integer.parseInt(txtHeight.getText().toString());
+                    Weightnumber = Integer.parseInt(txtWeight.getText().toString());
 
 
+                    String ChildFirstName = txtFirstName.getText().toString();
+                    child.setChildName(ChildFirstName);
 
 
+                    String ChildLastName = txtLastName.getText().toString();
+                    child.setChildLastName(ChildLastName);
 
+
+                    String ChildMotherName = txtMotherName.getText().toString();
+                    child.setChildMotherName(ChildMotherName);
+
+
+                    String Phone = txtPhone.getText().toString();
+                    child.setPhone(Phone);
+
+                    child.setBirthday(DateUtil.getDateFromString(BDate + " 00:00"));
+
+
+                    child.setBlood(typeofblood);
+
+                    final String strGender =
+                            ((RadioButton) findViewById(mGender.getCheckedRadioButtonId()))
+                                    .getText().toString();
+                    child.setGender(strGender);
+
+                    mGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                        @Override
+                        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                            mGenderOptions = mGender.findViewById(i);
+
+                            switch (i) {
+                                case R.id.r_Male:
+//                                strGender  = mGenderOptions.getText().toString();
+//                                child.setGender(strGender);
+                                    break;
+
+                                case R.id.r_Fmale:
+//                                strGender  = mGenderOptions.getText().toString();
+//                                child.setGender(strGender);
+
+                                    break;
+
+                                default:
+
+
+                            }
+                        }
+                    });
+
+                    user = FirebaseAuth.getInstance().getCurrentUser();
+                    String id = user.getUid();
+
+
+                    Map<String, Object> userh = new HashMap<>();
+                    userh.put("weight", Weightnumber);
+                    userh.put("height", Heightnumber);
+
+
+                    db.collection("child").document(id).set(child);
+
+                    LocalDate currentDate = LocalDate.now(ZoneId.systemDefault());
+
+                    db.collection("child").document(id).collection("IBM").document(id).set(userh);
+
+                    if (SpecialNeeds.isChecked()) {
+                        Map<String, Object> SpecialNeeds = new HashMap<>();
+                        SpecialNeeds.put("status", "Yes");
+                        SpecialNeeds.put("type", null);
+
+                        db.collection("child").document(id)
+                                .collection("Special Needs").document(id).set(SpecialNeeds);
+                    } else {
+//                    db.collection("child").document(id)
+//                            .collection("Special Needs").document(id).delete();
+                    }
+
+                    Toast.makeText(Register.this, "data inserted", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Register.this, MyDrawer.class);
+                    startActivity(intent);
 
 
                     Toast.makeText(Register.this, "data inserted", Toast.LENGTH_LONG).show();
 
-                    Intent intent = new Intent(Register.this, activity_Register1.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
-                    intent.putExtra("ChildFirstName", txtFirstName.getText().toString());
-                    intent.putExtra("ChildLastName", txtLastName.getText().toString());
-                    intent.putExtra("ChildMotherName", txtMotherName.getText().toString());
-                    intent.putExtra("Phone", txtPhone.getText().toString());
-                    startActivity(intent);
 
                 } catch (Exception e) {
                     Toast.makeText(Register.this, "Wrong input, please retry", Toast.LENGTH_SHORT).show();
@@ -236,14 +356,43 @@ public class Register extends AppCompatActivity {
 
             // the user has successfully picked an image
             // we need to save its reference to a Uri variable
-            pickedImgUri = data.getData() ;
+            pickedImgUri = data.getData();
             ImgUserPhoto.setImageURI(pickedImgUri);
 
+        }
+        }
+        public void setDate(View view) {
+            txtBirthday = (EditText) view;
 
+            datePicker = new DatePickerDialog(Register.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            int et_id = txtBirthday.getId();
+                            String date = (dayOfMonth) + "-" + (monthOfYear) + "-" + (year);
+
+                            if(et_id == R.id.editTextBabeBirthday) {
+                                BDate = date;
+                            }
+
+
+                            ProfileFragment fragment = new ProfileFragment();
+                            Bundle bundle = new Bundle();
+                            bundle.putString("birthDate", BDate);
+                            fragment.setArguments(bundle);
+
+                            txtBirthday.setText(date);
+                        }
+                    }, year, month, day);
+            datePicker.show();
+
+        }
+        public void ClickBackToLogin2(View view) {
+            Intent intent = new Intent(Register.this, Login_form.class);
+            startActivity(intent);
         }
 
 
-    }
 
     public void ClickBackToLogin(View view)
     {
